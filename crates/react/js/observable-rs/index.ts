0f500b25249566ff8239e03d8077bfa1417d9d8f
Daniel Norman <daniel@danielnorman.net>
2021-02-18 18:50:17 -0800

    Created npm helper package

---
+++ crates/react/js/observable-rs/index.ts
@@ -0,0 +1,14 @@
+import React, { useReducer, useEffect } from "react";
+
+export function useObserve(observable: any) {
+    // const [_, forceUpdate] = useReducer((x) => x + 1, 0);
+
+    // useEffect(() => {
+    // let unsub = observable.subscribe((v: any) => {
+    //     forceUpdate();
+    // });
+    // return () => {
+    //     unsub();
+    // };
+    // }, []);
+}
